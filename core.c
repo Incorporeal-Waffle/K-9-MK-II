21904ca9de1c68839d322b5c0c8677eb0f660144
Incorporeal-Waffle <Fuckyoutoo@plfgr.eu.org>
2016-07-20 01:11:10 +0300

    Stuff. Dunno.

--- core.c
+++ core.c
@@ -6,12 +6,50 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include <dirent.h>
+
+#define NPOLLFDS 2
+
+int loadModules(char *moduleDir){
+	DIR *mDir=opendir(moduleDir);
+	struct dirent *entt;
+	char *dirName;
+
+	printf("Loading modules from: %s\n", moduleDir);
+
+	if(mDir==NULL){
+		perror("opendir");
+		return 0;
+	}
+	
+	while((entt=readdir(mDir))!=NULL){
+		dirName=strdup(moduleDir);
+		dirName=realloc(dirName, strlen(dirName)+2+strlen(entt->d_name));
+		strcat(dirName, "/");
+		strcat(dirName, entt->d_name);
+		printf("Loading module: %s\n", dirName);
+		//loadModule(dirName);
+	}
+	
+	return 1;
+}
 
 int main(int argc, char **argv){
-	int opt;
+	int opt, rbytes;
 	struct addrinfo *host, hints;
+	struct pollfd pollfds[NPOLLFDS];
+	char *dirName;
+	char buf[513];
+	buf[512]='\0';
+	
+	gArgc=argc;
+	gArgv=argv;
 
-	while((opt=getopt(argc, argv, "Vhd:p:")) != -1){
+	while((opt=getopt(argc, argv, "Vhd:p:m:")) != -1){
 		switch(opt){
 			case 'V':
 				printf("%s\n", VERSION);
@@ -19,23 +57,44 @@ int main(int argc, char **argv){
 			case 'h':
 				printf("TODO: usage information\n");
 				return 0;
-			case 'd':
+			case 'd'://Host
 				serverHost=optarg;
 				break;
-			case 'p':
+			case 'p'://Port
 				serverPort=optarg;
+				break;
+			case 'm'://Module dir
+				moduleDir=optarg;
 				break;
 			default:
 				return 1;
 		}
 	}
+	//TODO: -v for verbose, make it silencier by default
 
 	if(serverHost==NULL)
 		serverHost="127.0.0.1";
 	if(serverPort==NULL)
 		serverPort="6667";
 	
-	printf("Host: %s\nPort: %s\n", serverHost, serverPort);
+	//Loading core modules
+	if(moduleDir){
+		dirName=strdup(moduleDir);
+		dirName=realloc(dirName, strlen(dirName)+1+strlen("/core"));//+1 for null
+		strcat(dirName, "/core");
+	}else{
+		dirName=strdup(gArgv[0]);
+		dirName=realloc(dirName, strlen(dirName)+1+strlen("/modules/core"));//+1 for null
+		dirName=dirname(dirName);//dirname modifies it's args
+		strcat(dirName, "/modules/core");
+	}
+
+	if(!loadModules(dirName)){
+		printf("Failed to load modules!\n");
+	}
+	free(dirName);
+
+	printf("Connecting to: %s:%s...\n", serverHost, serverPort);
 
 	hints.ai_family=AF_UNSPEC;
 	hints.ai_socktype=SOCK_STREAM;
@@ -60,7 +119,6 @@ int main(int argc, char **argv){
 			return 1;
 	}
 	
-
 	for(struct addrinfo *addr=host; addr!=NULL; addr=addr->ai_next){
 		sockfd=socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
 		if(connect(sockfd, addr->ai_addr, addr->ai_addrlen)){//Couldn't connect
@@ -68,14 +126,36 @@ int main(int argc, char **argv){
 			sockfd=-1;
 			continue;
 		}
-		break;//We've got a connection!
+		freeaddrinfo(host);
+		break;
 	}
 
 	if(sockfd==-1){
 		perror("connect");
-		printf("Could not connect.");
+		printf("Could not connect.\n");
 		return 1;
 	}
+
+	printf("We've got a connection!\n");
+
+	pollfds[0].fd=STDIN_FILENO;
+	pollfds[0].events=POLLIN;
+
+	pollfds[1].fd=sockfd;
+	pollfds[1].events=POLLIN;
 	
+	while(poll(pollfds, NPOLLFDS, -1)>0){
+		if(pollfds[1].revents){//sock
+			rbytes=read(sockfd, buf, 512);
+			write(STDOUT_FILENO, buf, rbytes);
+			buf[rbytes]=0;
+		}
+
+		if(pollfds[0].revents){//stdin
+			rbytes=read(STDIN_FILENO, buf, 512);
+			write(sockfd, buf, rbytes);
+		}
+	}
+
 	return 0;
 }
--- core.h
+++ core.h
@@ -1,7 +1,14 @@
 #define VERSION "0.0.0"
+#include "config.h"
 
-int sockfd;
+int sockfd, gArgc;
+char **gArgv;
 char *serverHost='\0';
 char *serverPort='\0';
+char *moduleDir='\0';
 
+//Functions
 int main(int argc, char **argv);
+int loadModule(char *modulePath);
+int loadModules(char *moduleDir);
+
